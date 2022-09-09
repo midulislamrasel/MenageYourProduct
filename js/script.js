8dad1eddffeba604c6256018698c4fcc7e8d0854
Asif-102 <asif1zero2@gmail.com>
2022-09-09 17:38:10 +0600

    initial part uploaded

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const inputValue=(id)=>{
+    const input = document.getElementById(id);
+    return input.value;
+}
+
+const addProduct=()=>{
+    const product_name = inputValue("product-name");
+    const product_quantity = inputValue("product-quantity");
+
+    // console.log(typeof product_name, Number.isInteger(Number(product_quantity)));
+}
