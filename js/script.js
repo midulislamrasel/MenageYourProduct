dc8ef910da886adf4ce6b6500bb8727719c4b98c
Asif-102 <asif1zero2@gmail.com>
2022-09-09 19:20:28 +0600

    some changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const inputValue=(id)=>{
+const inputValue = (id) => {
     const input = document.getElementById(id);
     return input.value;
 }
 
-const addProduct=()=>{
-    const product_name = inputValue("product-name");
+const addProduct = () => {
+    const product_name = inputValue("product-nam");
     const product_quantity = inputValue("product-quantity");
 
     // console.log(typeof product_name, Number.isInteger(Number(product_quantity)));
